60b7782d5f7ebbd2377b5ac2cdc3a9cfaac14a27
Thiago Marcelo Marques-TMARQUES <tmarques@motorola.com>
2022-08-05 01:37:55 -0400

    Hash funcionando ok

--- proj.py
+++ proj.py
@@ -1,11 +1,7 @@
 import re
 
+
 # autor, Senha, IP, email, transação, repositório, Hash
-
-def valida_email(email : str): # faltando
-    regex = re.compile(r"^[a-zA-Z][^@]*@.+\..+")
-
-
 def valida_autor(autor : str): # faltando
     regex = re.compile(r"^[a-zA-Z]\w*")
 
@@ -15,6 +11,12 @@ def valida_senha(senha : str): # acho que ta ok
     correspondencias = regex.search(senha)
     return correspondencias != None
 
+def valida_ip(ip : str): # faltando
+    regex = re.compile(r"")
+
+def valida_email(email : str): # faltando
+    regex = re.compile(r"^[a-zA-Z][^@]*@.+\..+")
+
 
 def valida_transacao(transacao : str): # acho que ta ok
     regex = re.compile(r"pull|push|stash|fork|pop")
@@ -22,17 +24,30 @@ def valida_transacao(transacao : str): # acho que ta ok
     return correspondencias != None
 
 
-def valida_repositorio(repositorio : str):
+def valida_repositorio(repositorio : str): #faltando
     regex = re.compile(r"^[a-z0-9]+_*[a-z0-9]+")
     # correspondencias = regex.search(repositorio)
     # print(correspondencias)
     # return correspondencias != None
+
+
+def valida_hash(hash : str): # acho que ta ok
+    regex = re.compile(r"^[a-f0-9]{32}$")
+    correspondencias = regex.search(hash)
+    return correspondencias != None
+
 
 valores = input().split(" ")
 
 if len(valores) != 7:
     print(False)
 
-print(valida_senha(valores[1]))
-print(valida_transacao(valores[4]))
-print(valida_repositorio(valores[5]))
+validacoes = []
+validacoes.append(valida_senha(valores[1]))
+validacoes.append(valida_transacao(valores[4]))
+validacoes.append(valida_hash(valores[6]))
+
+if(False in validacoes):
+    print(False)
+else:
+    print(True)
